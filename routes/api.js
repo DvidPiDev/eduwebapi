3ef6b09dceb6825f4c7849566d89e06dcb98b23a
dvidpi <trubka287@gmail.com>
2024-06-09 14:26:44 +0200

    finish all routes, remove method from env

--- routes/api.js
+++ routes/api.js
@@ -4,9 +4,9 @@ const {Edupage} = require('edupage-api');
 const edupage = new Edupage();
 
 // This parses the timeline in a way where only the important stuff is left.
-function parseTimeline(arr) {
+function parseTimeline(arr, amnt) {
     arr.sort((a, b) => new Date(b.creationDate) - new Date(a.creationDate));
-    const slice = arr.slice(0, 25);
+    const slice = arr.slice(0, amnt);
     const filter = slice.filter(obj => obj.type !== "homework");
     return filter.map(obj => {
         const merge = {
@@ -65,27 +65,277 @@ router.post('/all', async (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
     try {
+    try {
+        await edupage.login(username, password);
+    } catch(error) {
+        res.status(500).json({message: "Invalid username or password"});
+    }
+
+    const date = new Date();
+    const dateTomorrow = new Date(Date.now() + 24 * 60 * 60 * 1000);
+
+    const timetable = await edupage.getTimetableForDate(date);
+    const timetableTomorrow = await edupage.getTimetableForDate(dateTomorrow);
+    const gradesRaw = edupage.grades.reverse();
+    const lessons = timetable.lessons
+    const assignmentsRaw = lessons.reduce((arr, lesson) => (arr.push(...lesson.assignments), arr), []);
+    const assignments = assignmentsRaw.map(item => ({
+        short: item.subject.short,
+        name: item.subject.name,
+        desc: item.title
+    }));
+
+    const lessonsTomorrow = timetableTomorrow.lessons
+    const assignmentsRawTomorrow = lessonsTomorrow.reduce((arr, lesson) => (arr.push(...lesson.assignments), arr), []);
+    const assignmentsTomorrow = assignmentsRawTomorrow.map(item => ({
+        short: item.subject.short,
+        name: item.subject.name,
+        desc: item.title
+    }));
+
+    const timeline = parseTimeline(edupage.timelineItems, 25);
+    const grades = collectGrades(gradesRaw);
+
+    res.status(200).json({
+        username,
+        today: {
+            1:{
+                name: timetable.lessons[0] ? timetable.lessons[0].subject.name : '-',
+                short: timetable.lessons[0] ? timetable.lessons[0].subject.short : '-',
+                time: "8:00"
+            },
+            2:{
+                name: timetable.lessons[1] ? timetable.lessons[1].subject.name : '-',
+                short: timetable.lessons[1] ? timetable.lessons[1].subject.short : '-',
+                time: "8:55"
+            },
+            3:{
+                name: timetable.lessons[2] ? timetable.lessons[2].subject.name : '-',
+                short: timetable.lessons[2] ? timetable.lessons[2].subject.short : '-',
+                time: "9:50"
+            },
+            4:{
+                name: timetable.lessons[3] ? timetable.lessons[3].subject.name : '-',
+                short: timetable.lessons[3] ? timetable.lessons[3].subject.short : '-',
+                time: "10:45"
+            },
+            5:{
+                name: timetable.lessons[4] ? timetable.lessons[4].subject.name : '-',
+                short: timetable.lessons[4] ? timetable.lessons[4].subject.short : '-',
+                time: "11:50"
+            },
+            6:{
+                name: timetable.lessons[5] ? timetable.lessons[5].subject.name : '-',
+                short: timetable.lessons[5] ? timetable.lessons[5].subject.short : '-',
+                time: "12:45"
+            },
+            7:{
+                name: timetable.lessons[6] ? timetable.lessons[6].subject.name : '-',
+                short: timetable.lessons[6] ? timetable.lessons[6].subject.short : '-',
+                time: "14:00"
+            }
+        },
+        tomorrow: {
+            1:{
+                name: timetableTomorrow.lessons[0] ? timetableTomorrow.lessons[0].subject.name : '-',
+                short: timetableTomorrow.lessons[0] ? timetableTomorrow.lessons[0].subject.short : '-',
+                time: "8:00"
+            },
+            2:{
+                name: timetableTomorrow.lessons[1] ? timetableTomorrow.lessons[1].subject.name : '-',
+                short: timetableTomorrow.lessons[1] ? timetableTomorrow.lessons[1].subject.short : '-',
+                time: "8:55"
+            },
+            3:{
+                name: timetableTomorrow.lessons[2] ? timetableTomorrow.lessons[2].subject.name : '-',
+                short: timetableTomorrow.lessons[2] ? timetableTomorrow.lessons[2].subject.short : '-',
+                time: "9:50"
+            },
+            4:{
+                name: timetableTomorrow.lessons[3] ? timetableTomorrow.lessons[3].subject.name : '-',
+                short: timetableTomorrow.lessons[3] ? timetableTomorrow.lessons[3].subject.short : '-',
+                time: "10:45"
+            },
+            5:{
+                name: timetableTomorrow.lessons[4] ? timetableTomorrow.lessons[4].subject.name : '-',
+                short: timetableTomorrow.lessons[4] ? timetableTomorrow.lessons[4].subject.short : '-',
+                time: "11:50"
+            },
+            6:{
+                name: timetableTomorrow.lessons[5] ? timetableTomorrow.lessons[5].subject.name : '-',
+                short: timetableTomorrow.lessons[5] ? timetableTomorrow.lessons[5].subject.short : '-',
+                time: "12:45"
+            },
+            7:{
+                name: timetableTomorrow.lessons[6] ? timetableTomorrow.lessons[6].subject.name : '-',
+                short: timetableTomorrow.lessons[6] ? timetableTomorrow.lessons[6].subject.short : '-',
+                time: "14:00"
+            }
+        },
+        timeline,
+        assignments: {
+            assignments,
+            assignmentsTomorrow
+        },
+        grades
+    });
+
+    edupage.exit();
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: "Internal server error"});
+    }
+});
+
+router.post('/timetable', async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    try {
         if (!username || !password) {
-            res.status(400).json({message: `Invalid username or password`});
+            res.status(400).json({message: "Invalid username or password"})
         }
         try {
             await edupage.login(username, password);
-        } catch(error) {
-            res.status(400).json({message: `Invalid username or password`});
+        } catch (error) {
+            res.status(400).json({message: "Invalid username or password"});
         }
         const date = new Date();
         const dateTomorrow = new Date(Date.now() + 24 * 60 * 60 * 1000);
 
-        const timetable = edupage.getTimetableForDate(date);
-        const timetableTomorrow = edupage.getTimetableForDate(dateTomorrow);
-        const gradesRaw = edupage.grades.reverse(); // trust, this is better reversed :P
-        const lessons = timetable.lessons;
+        const timetable = await edupage.getTimetableForDate(date);
+        const timetableTomorrow = await edupage.getTimetableForDate(dateTomorrow);
+
+        res.status(200).json({
+            today: {
+                1:{
+                    name: timetable.lessons[0] ? timetable.lessons[0].subject.name : '-',
+                    short: timetable.lessons[0] ? timetable.lessons[0].subject.short : '-',
+                    time: "8:00"
+                },
+                2:{
+                    name: timetable.lessons[1] ? timetable.lessons[1].subject.name : '-',
+                    short: timetable.lessons[1] ? timetable.lessons[1].subject.short : '-',
+                    time: "8:55"
+                },
+                3:{
+                    name: timetable.lessons[2] ? timetable.lessons[2].subject.name : '-',
+                    short: timetable.lessons[2] ? timetable.lessons[2].subject.short : '-',
+                    time: "9:50"
+                },
+                4:{
+                    name: timetable.lessons[3] ? timetable.lessons[3].subject.name : '-',
+                    short: timetable.lessons[3] ? timetable.lessons[3].subject.short : '-',
+                    time: "10:45"
+                },
+                5:{
+                    name: timetable.lessons[4] ? timetable.lessons[4].subject.name : '-',
+                    short: timetable.lessons[4] ? timetable.lessons[4].subject.short : '-',
+                    time: "11:50"
+                },
+                6:{
+                    name: timetable.lessons[5] ? timetable.lessons[5].subject.name : '-',
+                    short: timetable.lessons[5] ? timetable.lessons[5].subject.short : '-',
+                    time: "12:45"
+                },
+                7:{
+                    name: timetable.lessons[6] ? timetable.lessons[6].subject.name : '-',
+                    short: timetable.lessons[6] ? timetable.lessons[6].subject.short : '-',
+                    time: "14:00"
+                }
+            },
+            tomorrow: {
+                1:{
+                    name: timetableTomorrow.lessons[0] ? timetableTomorrow.lessons[0].subject.name : '-',
+                    short: timetableTomorrow.lessons[0] ? timetableTomorrow.lessons[0].subject.short : '-',
+                    time: "8:00"
+                },
+                2:{
+                    name: timetableTomorrow.lessons[1] ? timetableTomorrow.lessons[1].subject.name : '-',
+                    short: timetableTomorrow.lessons[1] ? timetableTomorrow.lessons[1].subject.short : '-',
+                    time: "8:55"
+                },
+                3:{
+                    name: timetableTomorrow.lessons[2] ? timetableTomorrow.lessons[2].subject.name : '-',
+                    short: timetableTomorrow.lessons[2] ? timetableTomorrow.lessons[2].subject.short : '-',
+                    time: "9:50"
+                },
+                4:{
+                    name: timetableTomorrow.lessons[3] ? timetableTomorrow.lessons[3].subject.name : '-',
+                    short: timetableTomorrow.lessons[3] ? timetableTomorrow.lessons[3].subject.short : '-',
+                    time: "10:45"
+                },
+                5:{
+                    name: timetableTomorrow.lessons[4] ? timetableTomorrow.lessons[4].subject.name : '-',
+                    short: timetableTomorrow.lessons[4] ? timetableTomorrow.lessons[4].subject.short : '-',
+                    time: "11:50"
+                },
+                6:{
+                    name: timetableTomorrow.lessons[5] ? timetableTomorrow.lessons[5].subject.name : '-',
+                    short: timetableTomorrow.lessons[5] ? timetableTomorrow.lessons[5].subject.short : '-',
+                    time: "12:45"
+                },
+                7:{
+                    name: timetableTomorrow.lessons[6] ? timetableTomorrow.lessons[6].subject.name : '-',
+                    short: timetableTomorrow.lessons[6] ? timetableTomorrow.lessons[6].subject.short : '-',
+                    time: "14:00"
+                }
+            }
+        });
+
+        edupage.exit();
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: "Internal server error"});
+    }
+});
+
+router.post('/timeline/:amount?', async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    let amount = req.params.amount;
+    if (!amount) {
+        amount = 25;
+    }
+    try {
+        if (!username || !password) {
+            res.status(400).json({message: "Invalid username or password"});
+        }
+        try {
+            await edupage.login(username, password);
+        } catch (error) {
+            res.status(400).json({message: "Invalid username or password"});
+        }
+        const timeline = parseTimeline(edupage.timelineItems, amount);
+        res.status(200).json({timeline});
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: "Internal server error"});
+    }
+});
+
+router.post('/assignments', async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    try {
+        try {
+            await edupage.login(username, password);
+        } catch (error) {
+            res.status(400).json({message: "Invalid username or password"});
+        }
+        const date = new Date();
+        const dateTomorrow = new Date(Date.now() + 24 * 60 * 60 * 1000);
+
+        const timetable = await edupage.getTimetableForDate(date);
+        const timetableTomorrow = await edupage.getTimetableForDate(dateTomorrow);
+
+        const lessons = timetable.lessons
         const assignmentsRaw = lessons.reduce((arr, lesson) => (arr.push(...lesson.assignments), arr), []);
         const assignments = assignmentsRaw.map(item => ({
             short: item.subject.short,
             name: item.subject.name,
             desc: item.title
         }));
+
         const lessonsTomorrow = timetableTomorrow.lessons
         const assignmentsRawTomorrow = lessonsTomorrow.reduce((arr, lesson) => (arr.push(...lesson.assignments), arr), []);
         const assignmentsTomorrow = assignmentsRawTomorrow.map(item => ({
@@ -93,100 +343,39 @@ router.post('/all', async (req, res) => {
             name: item.subject.name,
             desc: item.title
         }));
-        const timeline = parseTimeline(edupage.timelineItems);
-        const grades = collectGrades(gradesRaw);
-        res.status(200).json({ // remember how the README said this is not production ready code? i wan't lying :D
-            username: username, // for confirmation
-            timetable: {
-                today: {
-                    1:{
-                        name: timetable.lessons[0] ? timetable.lessons[0].subject.name : '-',
-                        short: timetable.lessons[0] ? timetable.lessons[0].subject.short : '-',
-                        time: "8:00"
-                    },
-                    2:{
-                        name: timetable.lessons[1] ? timetable.lessons[1].subject.name : '-',
-                        short: timetable.lessons[1] ? timetable.lessons[1].subject.short : '-',
-                        time: "8:55"
-                    },
-                    3:{
-                        name: timetable.lessons[2] ? timetable.lessons[2].subject.name : '-',
-                        short: timetable.lessons[2] ? timetable.lessons[2].subject.short : '-',
-                        time: "9:50"
-                    },
-                    4:{
-                        name: timetable.lessons[3] ? timetable.lessons[3].subject.name : '-',
-                        short: timetable.lessons[3] ? timetable.lessons[3].subject.short : '-',
-                        time: "10:45"
-                    },
-                    5:{
-                        name: timetable.lessons[4] ? timetable.lessons[4].subject.name : '-',
-                        short: timetable.lessons[4] ? timetable.lessons[4].subject.short : '-',
-                        time: "11:50"
-                    },
-                    6:{
-                        name: timetable.lessons[5] ? timetable.lessons[5].subject.name : '-',
-                        short: timetable.lessons[5] ? timetable.lessons[5].subject.short : '-',
-                        time: "12:45"
-                    },
-                    7:{
-                        name: timetable.lessons[6] ? timetable.lessons[6].subject.name : '-',
-                        short: timetable.lessons[6] ? timetable.lessons[6].subject.short : '-',
-                        time: "14:00"
-                    }
-                },
-                tomorrow: {
-                    1:{
-                        name: timetableTomorrow.lessons[0] ? timetableTomorrow.lessons[0].subject.name : '-',
-                        short: timetableTomorrow.lessons[0] ? timetableTomorrow.lessons[0].subject.short : '-',
-                        time: "8:00"
-                    },
-                    2:{
-                        name: timetableTomorrow.lessons[1] ? timetableTomorrow.lessons[1].subject.name : '-',
-                        short: timetableTomorrow.lessons[1] ? timetableTomorrow.lessons[1].subject.short : '-',
-                        time: "8:55"
-                    },
-                    3:{
-                        name: timetableTomorrow.lessons[2] ? timetableTomorrow.lessons[2].subject.name : '-',
-                        short: timetableTomorrow.lessons[2] ? timetableTomorrow.lessons[2].subject.short : '-',
-                        time: "9:50"
-                    },
-                    4:{
-                        name: timetableTomorrow.lessons[3] ? timetableTomorrow.lessons[3].subject.name : '-',
-                        short: timetableTomorrow.lessons[3] ? timetableTomorrow.lessons[3].subject.short : '-',
-                        time: "10:45"
-                    },
-                    5:{
-                        name: timetableTomorrow.lessons[4] ? timetableTomorrow.lessons[4].subject.name : '-',
-                        short: timetableTomorrow.lessons[4] ? timetableTomorrow.lessons[4].subject.short : '-',
-                        time: "11:50"
-                    },
-                    6:{
-                        name: timetableTomorrow.lessons[5] ? timetableTomorrow.lessons[5].subject.name : '-',
-                        short: timetableTomorrow.lessons[5] ? timetableTomorrow.lessons[5].subject.short : '-',
-                        time: "12:45"
-                    },
-                    7:{
-                        name: timetableTomorrow.lessons[6] ? timetableTomorrow.lessons[6].subject.name : '-',
-                        short: timetableTomorrow.lessons[6] ? timetableTomorrow.lessons[6].subject.short : '-',
-                        time: "14:00"
-                    }
-                },
-            },
-            timeline,
-            assignments: {
-                assignments,
-                assignmentsTomorrow
-            },
-            grades
+
+
+        res.status(200).json({
+            assignments,
+            assignmentsTomorrow
         });
-        edupage.exit(); // almost forgot this
-    } catch (error) {
+    } catch(error) {
         console.error(error);
+        res.status(500).json({message: "Internal server error"});
     }
 });
 
-process.on('uncaughtException', (error) => {
+router.post('/grades', async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    try {
+        try {
+            await edupage.login(username, password);
+        } catch (error) {
+            res.status(400).json({message: "Invalid username or password"});
+        }
+
+        const gradesRaw = edupage.grades.reverse();
+        const grades = collectGrades(gradesRaw);
+
+        res.status(200).json({grades});
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: "Internal server error"});
+    }
+});
+
+process.on('uncaughtException', (error) => { // This should not be here but due to how EdupageAPI handles LoginErrors, it has to be here
     console.log("The node process is now in an undefined state, it can run but it's recommended to restart it, error:\n")
     console.error(error);
 });
